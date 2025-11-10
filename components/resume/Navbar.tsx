6f7a2fb4ba65a22a4ebbff12d91a823e6004cb06
Tejinder Muhar <tejinder@Mac.lan>
2025-11-10 22:33:48 +0530

    new updates...

--- components/resume/Navbar.tsx
+++ components/resume/Navbar.tsx
@@ -1,13 +1,13 @@
-import {Link} from "react-router";
+import Link from 'next/link'
 
 const Navbar = () => {
     return (<header className="bg-[#11224E] py-5 px-6 flex justify-center">
         <div className="w-full max-w-[1200px] flex justify-between items-center">
           <div className="flex items-center gap-2">
-             <Link to="/"><img src="/images/logo.png" alt="Resume Analyzer" className="h-8 w-auto" /></Link>
+             <Link href="/"><img src="/images/logo.png" alt="Resume Analyzer" className="h-8 w-auto" /></Link>
           </div>
           <Link
-            to="/upload"
+            href="/upload"
             className="bg-[#CE2D4F] hover:bg-[#b62646] text-white text-sm font-medium px-5 py-2 rounded-full transition-colors"
           >
             Upload Resume
